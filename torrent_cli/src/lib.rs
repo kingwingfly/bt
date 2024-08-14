1180502e96e042468de21aed25796425f03706bb
Louis <836250617@qq.com>
2024-08-15 01:49:00 +0800

    ci

--- torrent_cli/src/lib.rs
+++ torrent_cli/src/lib.rs
@@ -1,4 +1,8 @@
-//! A bittorrent client.
-
-#![deny(missing_docs)]
+#![doc = include_str!("../README.md")]
+#![deny(
+    missing_docs,
+    rustdoc::broken_intra_doc_links,
+    elided_lifetimes_in_paths
+)]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
+
