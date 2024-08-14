9b25b1c2d9e244a8e77d74ada75fbb051df5efcb
Louis <836250617@qq.com>
2024-08-15 01:00:40 +0800

    first commit

---
+++ .github/scripts/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+export TERM=xterm-256color
+
+# Statements waiting to be executed
+statements=(
+    "cargo clippy --no-default-features -- -D warnings"
+
+    "cargo test --no-default-features"
+
+    "cargo run --example example --no-default-features"
+
+    "cargo doc --no-deps --no-default-features"
+)
+
+# loop echo and executing statements
+for statement in "${statements[@]}"; do
+    echo "$(tput setaf 3)$statement$(tput sgr0)"
+    eval $statement
+    echo
+done
---
+++ torrent/src/lib.rs
@@ -0,0 +1,4 @@
+//! A bittorrent client.
+
+#![deny(missing_docs)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
